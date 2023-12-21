8c7f7a0e912dbfafe2b6b714ce9b0efbe9aaac9d
Parkjju <rudwns3927@gmail.com>
2023-12-21 18:48:28 +0900

    Initial Commit

---
+++ posepicker Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  posepicker Watch App
+//
+//  Created by Jun on 2023/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ posepicker Watch App/posepickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  posepickerApp.swift
+//  posepicker Watch App
+//
+//  Created by Jun on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct posepicker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
